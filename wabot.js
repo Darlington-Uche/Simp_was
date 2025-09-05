2a5587e8f1ce4bbbb174139b48e9af0171dd04c3
Darlington <darlingtonweb3@gmail.com>
2025-09-05 09:19:27 +0100

    Update wabot.js

--- wabot.js
+++ wabot.js
@@ -2,7 +2,7 @@
 // WhatsApp GC bot with anti-spam + token search by $SYMBOL
 // Requires: @whiskeysockets/baileys, pino, qrcode-terminal, axios, fs-extra
 
-const { default: makeWASocket, useMultiFileAuthState, jidNormalizedUser } = require("@whiskeysockets/baileys")
+cot { default: makeWASocket, useMultiFileAuthState, jidNormalizedUser } = require("@whiskeysockets/baileys")
 const P = require("pino")
 const qrcode = require("qrcode-terminal")
 const axios = require("axios")
